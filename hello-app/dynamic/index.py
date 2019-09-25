2058a606b2d12b5a60132c6dbb0898d3ec72470f
Roman <pidor@example.com>
2019-09-25 06:38:15 -0400

    Fix RDS connectivity

--- hello-app/dynamic/index.py
+++ hello-app/dynamic/index.py
@@ -5,41 +5,44 @@ import web
 import mysql.connector
 import boto3
 
-client = boto3.client('rds',region_name='us-east-1')
-token = client.generate_db_auth_token('test-database.c1eo8ftt0y1z.us-east-1.rds.amazonaws.com',3306,'ec2')
-
-print token
-
-mydb = mysql.connector.connect(
-    host = "test-database.c1eo8ftt0y1z.us-east-1.rds.amazonaws.com",
-    password = token,
-    ssl_ca = 'rds-combined-ca-bundle.pem',
-#    auth_plugin = 'mysql_clear_password',
-    user = 'ec2'
-)
-
-cursor = mydb.cursor()
 
 urls = ("/dynamic", "hello")
 app = web.application(urls, globals())
 
 class hello:
-	def GET(self):
-                index_query = ("SECLECT count FROM inexes")
+        def GET(self):
+                client = boto3.client('rds',region_name='us-east-1')
+                token = client.generate_db_auth_token('test-database.c1eo8ftt0y1z.us-east-1.rds.amazonaws.com',3306,'ec2')
+
+                mydb = mysql.connector.connect(
+                    host = "test-database.c1eo8ftt0y1z.us-east-1.rds.amazonaws.com",
+                    password = token,
+                    ssl_ca = 'rds-combined-ca-bundle.pem',
+               #    auth_plugin = 'mysql_clear_password',
+                    user = 'ec2',
+                    database = 'testdb'
+                )
+
+                cursor = mydb.cursor(buffered=True)
+
+                index_query = "SELECT count FROM indexes"
                 cursor.execute(index_query)
-                index = cursor.count
-                new_index= inex + 1
-                add_index_query = ("INSERT INTO indexes (count) VALUES (%s)", new_index)
-                cursor.execute(add_index_query)
-                cursor.commit()
-		return("""<html>
+                new_index = int(cursor.rowcount) + 1
+                mydb.commit()
+                add_index_query = "INSERT INTO indexes (count) VALUES (%s)"
+                new = (new_index,)
+                cursor.execute(add_index_query, new)
+                mydb.commit()
+                mydb.close()
+                return """<html>
 <header><title>Dynamic page</title></header>
 <body>
-Vists number: %s
-Hello World from dynamic page!
+<h1>Vists number: {}</h1>
+<p>Hello World from dynamic page</p>
 </body>
-</html>""", new_index)
+</html>""".format(new_index)
 
 if __name__ == "__main__":
-	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
-	app.run()
+        web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+        app.run()
+
